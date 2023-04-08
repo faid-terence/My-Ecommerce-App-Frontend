13a06f4a0c458595d0c8bfb3e828ef11075b1899
Faid Terence JABO <faidterence@outlook.com>
2023-04-08 14:39:31 +0200

    feature(ecommerce app): Design Landing Page

---
+++ script.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar) {
+    bar.addEventListener('click', () => {
+        navbar.classList.add('active');
+    })
+}
+
+
+if(close) {
+    close.addEventListener('click', () => {
+        navbar.classList.remove('active');
+    })
+}
